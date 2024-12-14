a95d0f57cab2c0229af92b29076c9345b765f847
Jestoni <jhontobit16@gmail.com>
2024-12-15 02:19:15 +0800

    first commit1

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const userRoutes = require('./routes/user.route');
-const clientRoutes = require('./routes/client.route');
-const listRoutes = require('./routes/list.route');
 const cors = require('cors');
+const userRoutes = require('../routes/user.route');
+const clientRoutes = require('../routes/client.route');
+const listRoutes = require('../routes/list.route');
 
 const app = express();
 
 // Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 // Routes
@@ -35,11 +33,15 @@ const connectToDatabase = async () => {
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
-    process.exit(1); // Exit the application if the database connection fails
+    throw error;
   }
 };
 
-// Start the server after connecting to the database
-
-
-connectToDatabase();
+module.exports = async (req, res) => {
+  try {
+    await connectToDatabase(); // Ensure DB connection
+    app(req, res); // Pass requests to the Express app
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
+};
