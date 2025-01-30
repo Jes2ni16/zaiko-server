2f640951d79de568c08f9327c6cfb7f10e2cfdc6
Jes2ni16 <jhontobit16@gmail.com>
2025-01-31 03:00:40 +0800

    add projects

--- models/project.model.js
+++ models/project.model.js
@@ -1,61 +1,46 @@
 const mongoose = require('mongoose');
 
 const propertySchema = new mongoose.Schema({
-  projectUrl:{
-    type:String,
-    default: ''
+  projectUrl: {
+    type: String,
+    default: '',
   },
   title: { type: String },
-  description: { type: String},
+  description: { type: String },
+  
   location: {
-    locationText:{
-      type:String
-    },
-    description1: {
-      title: { type: String },
-      ul: [{ type: String }],
-    },
-    description2: {
-      title: { type: String },
-      ul: [{ type: String }],
-    },
-    description3: {
-      title: { type: String },
-      ul: [{ type: String }],
-    },
-    description4: {
-      title: { type: String },
-      ul: [{ type: String }],
-    },
+    locationText: { type: String }, // for the main location text
+    descriptions: [
+      {
+        title: { type: String },
+        ul: [{ type: String }],
+      }
+    ], // array of descriptions
     img: { type: String },
   },
+  
   projectDetails: {
     ul: [{ type: String }],
     imgs: [{ type: String }],
   },
+  
   amenitiesFacilities: {
     description: { type: String },
     ul: [{ type: String }],
     imgs: [{ type: String }],
   },
+  
   unitDetails: {
     text: { type: String },
-    details1: {
-      title: { type: String },
-      ul: [{ type: String }],
-      imgs: [{ type: String }],
-    },
-    details2: {
-      title: { type: String },
-      ul: [{ type: String }],
-      imgs: [{ type: String }],
-    },
-    details3: {
-      title: { type: String },
-      ul: [{ type: String }],
-      imgs: [{ type: String }],
-    },
+    details: [
+      {
+        title: { type: String },
+        ul: [{ type: String }],
+        imgs: [{ type: String }],
+      }
+    ], 
   },
+  
   siteUpdate: {
     title: { type: String },
     imgs: [{ type: String }],
