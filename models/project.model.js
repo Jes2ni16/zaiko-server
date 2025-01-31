2c1daa59e06fca4c26a85b37f9ac8c6acf050abc
Jes2ni16 <jhontobit16@gmail.com>
2025-02-01 01:47:22 +0800

    add projects

--- models/project.model.js
+++ models/project.model.js
@@ -5,53 +5,54 @@ const propertySchema = new mongoose.Schema({
     type: String,
     default: '',
   },
-  projectImg: { type: String, default: '' },
-  projectLocation: { type: String, default: '' },
-  title: { type: String, required: false },
-  description: { type: String, default: '' },
-
+  projectImg: { type: String },
+  projectLocation: { type: String },
+  title: { type: String },
+  description: { type: String },
+  
   location: {
-    locationText: { type: String, default: '' }, // Main location text
+    locationText: { type: String }, // for the main location text
     descriptions: [
       {
-        title: { type: String, default: '' },
-        ul: [{ type: String, default: '' }], // List items
-      },
-    ],
-    img: { type: String, default: '' },
+        title: { type: String },
+        ul: [{ type: String }],
+      }
+    ], // array of descriptions
+    img: { type: String },
   },
-
+  
   projectDetails: {
-    ul: [{ type: String, default: '' }],
-    imgs: [{ type: String, default: '' }],
+    ul: [{ type: String }],
+    imgs: [{ type: String }],
   },
-
+  
   amenitiesFacilities: {
-    description: { type: String, default: '' },
-    ul: [{ type: String, default: '' }],
-    imgs: [{ type: String, default: '' }],
+    description: { type: String },
+    ul: [{ type: String }],
+    imgs: [{ type: String }],
   },
-
+  
   unitDetails: {
-    text: { type: String, default: '' },
+    text: { type: String },
     details: [
       {
-        title: { type: String, default: '' },
-        ul: [{ type: String, default: '' }],
-        imgs: [{ type: String, default: '' }],
-      },
-    ],
+        title: { type: String },
+        ul: [{ type: String }],
+        imgs: [{ type: String }],
+      }
+    ], 
   },
 
   unitDeliverable: {
-    text: { type: String, default: '' },
-    ul: [{ type: String, default: '' }],
-    imgs: [{ type: String, default: '' }],
-  },
+    text: { type: String },
+        ul: [{ type: String }],
+        imgs: [{ type: String }],
+      },
 
+  
   siteUpdate: {
-    title: { type: String, default: '' },
-    imgs: [{ type: String, default: '' }],
+    title: { type: String },
+    imgs: [{ type: String }],
   },
 });
 
