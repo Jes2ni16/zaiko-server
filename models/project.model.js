dce59a368b6139f64cae27857e3002831f4609fa
Jes2ni16 <jhontobit16@gmail.com>
2025-02-05 00:54:29 +0800

    add projects

--- models/project.model.js
+++ models/project.model.js
@@ -5,72 +5,9 @@ const propertySchema = new mongoose.Schema({
     type: String,
     default: '',
   },
-  projectImg: { type: String },
-  projectLocation: { type: String },
-  title: { type: String },
-  description: { type: String },
-  
-  location: {
-    locationText: { type: String }, // for the main location text
-    descriptions: [
-      {
-        title: { type: String },
-        ul: [{ type: String }],
-      }
-    ], // array of descriptions
-    img: { type: String },
-  },
-  
-  projectDetails: {
-    descriptions: [
-      {
-        title: { type: String },
-        ul: [{ type: String }],
-        imgs: [{ type: String }],
-      }
-    ],
-  },
-  
-  amenitiesFacilities: {
-    description: { type: String },
-    ul: [{ type: String }],
-    imgs: [{ type: String }],
-  },
-  
-  unitDetails: {
-    text: { type: String },
-    details: [
-      {
-        title: { type: String },
-        ul: [{ type: String }],
-        imgs: [{ type: String }],
-      }
-    ], 
-  },
-
-  buildingFeatures: {
-    text: { type: String },
-    details: [
-      {
-        title: { type: String },
-        ul: [{ type: String }],
-      }
-    ], 
-  },
-
-  unitDeliverable: {
-    text: { type: String },
-        ul: [{ type: String }],
-        imgs: [{ type: String }],
-      },
-      floorPlan:{
-        type:[String]
-      },
-  
-  siteUpdate: {
-    title: { type: String },
-    imgs: [{ type: String }],
-  },
+projectDocuments: {
+  type: String,
+}
 });
 
 const Property = mongoose.model('Property', propertySchema);
